024f002d1c7e5b2521695ff44a8694aec90072fb
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-04 17:06:54 -0400

    commit tree e31314e3382b0c8117375a9b19d6a08b100f750c

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 12;
+let num4 = 3;
+
+const mod = num3 / num4;
+
+const max = Math.max(1, 3, 4, 15, 7, 4, 20);
